68b86b17f4fa4f3bfd5c6c8d04a3c51c61bdf381
wq <wqkant@gmail.com>
2019-02-20 13:59:16 +0800

    #:练习

--- src/main/scala/practise/chapter3/chapter3datastructures.scala
+++ src/main/scala/practise/chapter3/chapter3datastructures.scala
@@ -178,7 +178,43 @@ object List {
     foldRight(as, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
 
   def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
-    Nil
+    concat(map(as)(f))
+
+  def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] =
+    flatMap(l)(a => if (f(a)) List(a) else Nil)
+
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwise(t1, t2))
+  }
+
+  /*
+  This function is usually called `zipWith`.
+  The discussion about stack usage from the explanation of `map` also
+  applies here. By putting the `f` in the second argument list,
+  Scala can infer its type from the previous argument list.
+  */
+  def zipWith[A, B, C](a: List[A], b: List[B])(f: (A, B) => C): List[C] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+  }
+
+  @annotation.tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l, prefix) match {
+    case (_, Nil) => true
+    case (Cons(h, t), Cons(h2, t2)) if h == h2 => startsWith(t, t2)
+    case _ => false
+  }
+
+  @annotation.tailrec
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil => sub == Nil
+    case _ if startsWith(sup, sub) => true
+    case Cons(h, t) => hasSubsequence(t, sub)
+  }
+
 
   //在伴生对象中定义一个可变参数的apply方法以便构造这个数据类型的实例是一种惯例
   def apply[A](as: A*): List[A] =
