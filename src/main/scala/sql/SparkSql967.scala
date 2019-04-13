35fe8ee2b9cdc277a7821b436a45f5240f9aa6e2
wq <wqkant@gmail.com>
2019-04-13 11:29:34 +0800

    add 967

--- src/main/scala/sql/SparkSql967.scala
+++ src/main/scala/sql/SparkSql967.scala
@@ -4,6 +4,10 @@ import java.io.File
 
 import org.apache.spark.sql.SparkSession
 import utils.FileUtils
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import org.apache.spark.sql.functions.{collect_list, udf, lit}
+
 
 object SparkSql967 extends App {
   FileUtils.dirDel(new File("D:/result"))
@@ -11,6 +15,8 @@ object SparkSql967 extends App {
     .builder()
     .master("local[*]")
     .getOrCreate()
+
+  import spark.implicits._
 
   spark
     .read
@@ -26,9 +32,9 @@ object SparkSql967 extends App {
          | select
          |   oi.user_id,
          |   case
-         |     when sum(oi.shipping_fee + oi.goods_amount) / count(1) <= 1/2 * ${avg} then '0-1/2'
-         |     when sum(oi.shipping_fee + oi.goods_amount) / count(1) <= ${avg} then '1/2-1'
-         |     when sum(oi.shipping_fee + oi.goods_amount) / count(1) <= 2 * ${avg} then '1-2'
+         |     when sum(oi.shipping_fee + oi.goods_amount) / count(1) <= 1/2 * ${avg} then '0-1/2_price'
+         |     when sum(oi.shipping_fee + oi.goods_amount) / count(1) <= ${avg} then '1/2-1_price'
+         |     when sum(oi.shipping_fee + oi.goods_amount) / count(1) <= 2 * ${avg} then '1-2_price'
          |     else '2-'
          |    end as avg
          | from order_info oi
@@ -89,13 +95,21 @@ object SparkSql967 extends App {
 
   spark
     .sql(
-      s"""
-         | select
-         |   oi.user_id,
-         |   count(1) as freq
-         | from order_info oi
-         | group by user_id
-          """.stripMargin)
+      """
+        | select
+        |   oi.user_id,
+        |   case
+        |    when count(1) = 1 then '1_ordered'
+        |    when count(1) = 2 then '2_ordered'
+        |    when count(1) = 3 then '3_ordered'
+        |    when count(1) = 4 then '4_ordered'
+        |    when count(1) = 5 then '5_ordered'
+        |   else
+        |    'more_than_5_ordered'
+        |   end as freq
+        | from order_info oi
+        | group by user_id
+      """.stripMargin)
     .createOrReplaceTempView("frequent")
 
 
@@ -104,7 +118,7 @@ object SparkSql967 extends App {
       s"""
          | select
          |   *
-         | from order_info oi
+         | from order_info
          |   inner join consume using (user_id)
          |   inner join activity using (user_id)
          |   inner join frequent using (user_id)
@@ -118,23 +132,172 @@ object SparkSql967 extends App {
 
 }
 
+
 object SparkSql967_1 extends App {
   FileUtils.dirDel(new File("D:/result"))
+
+
   val spark = SparkSession
     .builder()
     .master("local[*]")
     .getOrCreate()
+
+  import spark.implicits._
+
   spark
     .read
     .option("header", "true")
     .option("delimiter", ",")
     .csv("D:\\user_tag.csv")
-    .select("device_id")
-    .distinct()
-    .coalesce(1)
-    .write
+    .createOrReplaceTempView("users")
+
+  spark
+    .sql(
+      s"""
+         | select
+         |  device_id,
+         |  first(avg) AS avg,
+         |  first(intervals) AS intervals,
+         |  first(freq) AS freq
+         | from
+         |  users
+         | group by device_id
+    """.stripMargin)
+    .createOrReplaceTempView("user_tag")
+
+
+  val visits = spark
+    .read
     .option("header", "true")
     .option("delimiter", ",")
-    .csv("d:\\result")
+    .csv("D:\\967.csv")
 
+  val login = spark
+    .read
+    .option("header", "true")
+    .option("delimiter", ",")
+    .csv("D:\\login.csv")
+
+
+  val dateStr = "2019-04-01"
+  val pattern = "yyyy-MM-dd"
+  val fmt = new SimpleDateFormat(pattern)
+  val calendar = Calendar.getInstance();
+  calendar.setTime(fmt.parse(dateStr))
+  (0 to 9).foreach(i => {
+
+
+    calendar.add(Calendar.DATE, i)
+    val d = fmt.format(calendar.getTime)
+    visits
+      .filter(s"date like '$d%'")
+      .createOrReplaceTempView("visits")
+    login
+      .filter(s"action_date = '$d'")
+      .createOrReplaceTempView("login")
+
+    spark
+      .sql(
+        s"""
+           | select
+           |  ug.*,
+           |  tmp.device_id,
+           |  page_code
+           | from
+           | (select
+           |   *,
+           |  row_number()
+           |   over (partition by device_id order by date desc)
+           |     as rank
+           | from visits
+           | ) as tmp
+           |  inner join login using(device_id)
+           |  inner join user_tag AS ug using(device_id)
+           | where tmp.rank = 1
+    """.stripMargin)
+      .createOrReplaceTempView("tmp")
+
+    val total = spark
+      .sql(
+        s"""
+           | select
+           |  count(*) AS nums
+           | from tmp
+    """.stripMargin)
+      .first()
+      .getLong(0)
+
+    if (total > 0) {
+      List("avg", "intervals", "freq") foreach (key => {
+        spark
+          .sql(
+            s"""
+               | select
+               |  ${key},
+               |  round(count(1)/${total}, 4) rate
+               | from tmp
+               | group by ${key}
+    """.stripMargin)
+          .createOrReplaceTempView("rates")
+        spark
+          .sql(
+            s"""
+               | select
+               |  ${key},
+               |  page_code,
+               |  count(1) as nums
+               | from tmp
+               | group by ${key}, page_code
+    """.stripMargin)
+          .createOrReplaceTempView("user_info")
+
+        spark
+          .sql(
+            s"""
+               | select
+               |  ${key},
+               |  concat_ws('|',collect_set(page_code)) AS page_codes
+               | from
+               | (select
+               |   *,
+               |  row_number()
+               |   over (partition by ${key}, page_code order by nums desc)
+               |     as rank
+               | from user_info
+               | ) as tmp
+               | where tmp.rank <= 10
+               | group by ${key}
+               |
+    """.stripMargin)
+          .createOrReplaceTempView("page_codes_t")
+
+        spark
+          .sql(
+            s"""
+               | select
+               |  ${key},
+               |  rate,
+               |  page_codes
+               | from rates
+               |  inner join page_codes_t using(${key})
+    """.stripMargin)
+          .createOrReplaceTempView(s"tmp_${key}")
+      })
+      spark
+        .sql(
+          s"""
+             | select
+             |  *
+             | from tmp_avg
+             |  union all select * from tmp_intervals
+             |  union all select * from tmp_freq
+    """.stripMargin)
+        .coalesce(1)
+        .coalesce(1)
+        .write
+        .option("header", "true")
+        .option("delimiter", ",")
+        .csv(s"d:\\$d")
+    }
+  })
 }
